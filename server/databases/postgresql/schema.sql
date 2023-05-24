729a0b3c30fcc699325ecef6dcf5a330aa0aac76
davidguy3237 <davidtran3237@gmail.com>
2023-05-24 15:01:14 -0700

    Move databases inside server directory & add cart table

--- server/databases/postgresql/schema.sql
+++ server/databases/postgresql/schema.sql
@@ -56,6 +56,14 @@ CREATE TABLE related (
  PRIMARY KEY (id)
 );
 
+CREATE TABLE cart (
+  id BIGSERIAL,
+  user_session INTEGER NOT NULL,
+  sku_id INTEGER NOT NULL,
+  count INTEGER NOT NULL,
+  PRIMARY KEY (id)
+);
+
 ALTER TABLE products ADD CONSTRAINT products_category_id_fkey FOREIGN KEY (category_id) REFERENCES categories(id);
 ALTER TABLE features ADD CONSTRAINT features_products_id_fkey FOREIGN KEY (products_id) REFERENCES products(id);
 ALTER TABLE styles ADD CONSTRAINT styles_products_id_fkey FOREIGN KEY (products_id) REFERENCES products(id);
