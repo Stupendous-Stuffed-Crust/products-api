f3dca928d68aef03cd5551d4e82aaf5df73271d5
davidguy3237 <davidtran3237@gmail.com>
2023-06-01 12:51:06 -0700

    Add route for loader.io

--- server/index.js
+++ server/index.js
@@ -9,6 +9,9 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 
+app.get('/loaderio-9b041f286395fb997d272d43dc939150', (req, res) => {
+  res.send('loaderio-9b041f286395fb997d272d43dc939150');
+});
 app.get('/products/:product_id/related', controllers.getRelated);
 app.get('/products/:product_id/styles', controllers.getStyles);
 app.get('/products/:product_id', controllers.getProductDetails);
